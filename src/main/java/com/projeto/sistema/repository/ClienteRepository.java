6ecf015b817691f7d5ea9b32ddff5d1971f77bf7
Alan Silva <alan.sousa@cittamobi.com.br>
2025-07-24 13:59:55 -0300

    Criação do Cliente, Cidade, Fornecedor, Funcionario front-end e back-end

---
+++ src/main/java/com/projeto/sistema/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.sistema.repository;
+
+
+import com.projeto.sistema.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
